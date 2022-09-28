d6be2c4d08b10473a8959802c5d3d2f97c3752a7
Henrik Skog <skog@Henriks-MBP.home>
2022-09-28 23:19:02 +0200

    feat: working workflow

---
+++ workflow.py
@@ -0,0 +1,77 @@
+from __future__ import print_function
+import json
+
+import os.path
+import sys
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+DEFAULT_NUMBER_RESULTS = 5
+
+def build_alfred_option(uid, title, arg):
+    return {"uid": uid, "title": title, "arg": arg}
+
+def transform(items):
+    alfred_items = [build_alfred_option(item['id'], item['name'], item['id']) for item in items]
+    return json.dumps({"items": alfred_items})
+
+def get_recent_docs(creds, number_of_docs):
+    service = build('drive', 'v3', credentials=creds)
+
+    # Call the Drive v3 API
+    results = service.files().list(pageSize=int(number_of_docs), fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        return []
+
+    return items
+
+
+def main():
+    creds = None
+
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+
+    try:
+
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+        # If there are no (valid) credentials available, let the user log in.
+
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+    except:
+        print(build_alfred_option(0, "An error occured while authenticating", ""))
+
+
+    try:
+        number_of_recent = DEFAULT_NUMBER_RESULTS
+        if len(sys.argv) > 1:
+            number_of_recent = sys.argv[1]
+
+        items = get_recent_docs(creds, number_of_recent)
+        print(transform(items))
+
+    except HttpError:
+        print(build_alfred_option(0, "Could not connect with the Google API", ""))
+
+if __name__ == '__main__':
+    main()
